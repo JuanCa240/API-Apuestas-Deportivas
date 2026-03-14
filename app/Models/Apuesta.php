9b2d59a66aae749fdee17154e9b21ab6526ba795
Juan Camilo Pulgarin <juancapulgarin9@gmail.com>
2026-03-14 18:23:42 -0500

    Se terminó de agregar los campos necesarios para el modelo 'Apuesta'

--- app/Models/Apuesta.php
+++ app/Models/Apuesta.php
@@ -6,5 +6,31 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Apuesta extends Model{
+    use HasFactory;
 
+    protected $fillable = [
+        'usuario_id',
+        'evento_id',
+        'tipo_apuesta',
+        'monto',
+        'cuota',
+        'estado',
+        'ganancia'
+    ];
+
+    protected $casts = [
+        'monto' => 'float',
+        'cuota' => 'float',
+        'ganancia' => 'float',
+    ];
+
+    // Relación con Usuario (User 1:N Apuestas)
+    public function usuario(){
+        return $this->belongsTo(User::class);
+    }
+
+    // Relación con Evento (Evento 1:N Apuestas)
+    public function evento(){
+        return $this->belongsTo(Evento::class);
+    }
 }
