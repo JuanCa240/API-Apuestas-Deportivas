ff97953452777636692fbaefcb4f79d1fc0c2dba
Juan Camilo Pulgarin <juancapulgarin9@gmail.com>
2026-03-15 10:19:31 -0500

    Se rellenaron los campos de evento

--- app/Models/Evento.php
+++ app/Models/Evento.php
@@ -1,9 +1,32 @@
 <?php
 
 namespace App\Models;
+
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Evento extends Model{
+    use HasFactory;
 
+    protected $fillable = [
+        'deporte',
+        'equipo_local',
+        'equipo_visitante',
+        'fecha',
+        'estado'
+    ];
+
+    protected $casts = [
+        'fecha' => 'datetime'
+    ];
+
+    // Un evento puede tener muchas apuestas
+    public function apuestas(){
+        return $this->hasMany(Apuesta::class);
+    }
+
+    // Un evento tiene un resultado
+    public function resultado(){
+        return $this->hasOne(Resultado::class);
+    }
 }
