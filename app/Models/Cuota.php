0038206759fcff57eca01bf4e6ce4efb5dbb0d48
Juan Camilo Pulgarin <juancapulgarin9@gmail.com>
2026-03-15 10:40:38 -0500

    Se rellenaron los campos de evento

--- app/Models/Cuota.php
+++ app/Models/Cuota.php
@@ -6,5 +6,20 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Cuota extends Model{
-    
+    use HasFactory;
+
+    protected $fillable = [
+        'evento_id',
+        'tipo_apuesta',
+        'cuota'
+    ];
+
+    protected $casts = [
+        'cuota' => 'float'
+    ];
+
+    // La cuota pertenece a un evento
+    public function evento(){
+        return $this->belongsTo(Evento::class);
+    }
 }
