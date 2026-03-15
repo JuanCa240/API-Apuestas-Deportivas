236f427136ad189507191acfd93eb3c66953448f
Juan Camilo Pulgarin <juancapulgarin9@gmail.com>
2026-03-15 11:44:45 -0500

    Se modifico un endpoint y una importacion

--- routes/api.php
+++ routes/api.php
@@ -1,13 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\AuthController;
-
+use App\Http\Controllers\Auth\AuthController;
 
 Route::prefix('auth')->group(function(){
 
     Route::post('/register', [AuthController::class,'register']);
     Route::post('/login', [AuthController::class,'login']);
-    Route::post('/verify-otp', [AuthController::class,'verifyOtp']);
+    Route::post('/verify-otp', [AuthController::class,'verificarOtp']);
 
 });
