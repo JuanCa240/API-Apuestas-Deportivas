e78f3f3851861bdcef4602338ec7a3ce8001b617
Guppy <alejo.diaz.ruiz@gmail .com>
2026-03-15 22:04:08 -0500

    se agregaron métodos dentro del controller

--- app/Http/Controllers/User/ApuestaController.php
+++ app/Http/Controllers/User/ApuestaController.php
@@ -6,5 +6,24 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 class ApuestaController extends Controller{
-    //
+    public function store(Request $request)
+    {
+        return response()->json(
+            $this->apuestaService->crearApuesta($request->all())
+        );
+    }
+
+    public function misApuestas()
+    {
+        return response()->json(
+             $this->apuestaService->misApuestas()
+        );
+    }
+
+    public function show($id)
+    {
+        return response()->json(
+            $this->apuestaService->detalleApuesta($id)
+        );
+    }
 }
