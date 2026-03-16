1a4c63b39e0c750a823df993736d6c6aa247ba6f
Guppy <alejo.diaz.ruiz@gmail .com>
2026-03-15 23:29:00 -0500

    se agregaron nuevas rutas SALDO dentro del middleware

--- routes/api.php
+++ routes/api.php
@@ -5,38 +5,42 @@ use App\Http\Controllers\Auth\AuthController;
 use App\Http\Controllers\Admin\EventoController;
 use App\Http\Controllers\Admin\ApuestaController;
 use App\Http\Controllers\Admin\CuotaController;
+use App\Http\Controllers\Admin\SaldoController;
 
-Route::prefix('auth')->group(function(){
+Route::prefix('auth')->group(function () {
 
     // AUTH
-    Route::post('/register', [AuthController::class,'register']);
-    Route::post('/login', [AuthController::class,'login']);
-    Route::post('/verify-otp', [AuthController::class,'verificarOtp']);
-
+    Route::post('/register', [AuthController::class, 'register']);
+    Route::post('/login', [AuthController::class, 'login']);
+    Route::post('/verify-otp', [AuthController::class, 'verificarOtp']);
 });
 
-   // MIDDLEWARE
+// MIDDLEWARE
 
-Route::middleware('auth:api')->group(function(){
+Route::middleware('auth:api')->group(function () {
 
-    Route::get('/me', [AuthController::class,'me']);
+    Route::get('/me', [AuthController::class, 'me']);
 
     // EVENTOS
-    Route::get('/eventos',[EventoController::class,'index']);
-    Route::post('/eventos',[EventoController::class,'store']);
-    Route::get('/eventos/{id}', [EventoController::class,'show']);
-    Route::put('/eventos/{id}', [EventoController::class,'update']);
-    Route::delete('/eventos/{id}', [EventoController::class,'destroy']);
-    Route::post('/apuestas',[ApuestaController::class,'store']);
-    Route::get('/mis-apuestas',[ApuestaController::class,'misApuestas']);
-    Route::get('/apuestas/{id}',[ApuestaController::class,'show']);
-    Route::put('/eventos/{id}/resultado',[EventoController::class,'resultado']);
+    Route::get('/eventos', [EventoController::class, 'index']);
+    Route::post('/eventos', [EventoController::class, 'store']);
+    Route::get('/eventos/{id}', [EventoController::class, 'show']);
+    Route::put('/eventos/{id}', [EventoController::class, 'update']);
+    Route::delete('/eventos/{id}', [EventoController::class, 'destroy']);
+    Route::post('/apuestas', [ApuestaController::class, 'store']);
+    Route::get('/mis-apuestas', [ApuestaController::class, 'misApuestas']);
+    Route::get('/apuestas/{id}', [ApuestaController::class, 'show']);
+    Route::put('/eventos/{id}/resultado', [EventoController::class, 'resultado']);
 
 
     // CUOTAS
-    Route::post('/cuotas',[CuotaController::class,'store']);
-    Route::get('/eventos/{evento}/cuotas',[CuotaController::class,'cuotasPorEvento']);
-    Route::put('/cuotas/{id}',[CuotaController::class,'update']);
-    Route::delete('/cuotas/{id}',[CuotaController::class,'destroy']);
+    Route::post('/cuotas', [CuotaController::class, 'store']);
+    Route::get('/eventos/{evento}/cuotas', [CuotaController::class, 'cuotasPorEvento']);
+    Route::put('/cuotas/{id}', [CuotaController::class, 'update']);
+    Route::delete('/cuotas/{id}', [CuotaController::class, 'destroy']);
 
+    // SALDO
+    Route::get('/saldo', [SaldoController::class, 'saldo']);
+    Route::post('/depositar', [SaldoController::class, 'depositar']);
+    Route::post('/retirar', [SaldoController::class, 'retirar']);
 });
